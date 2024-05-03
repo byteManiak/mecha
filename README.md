22df4a20be24bb964b619300242bdb6782e01335
bytemaniak <bytemaniak98@gmail.com>
2024-05-03 16:37:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# MECHA
+
+![MECHA Logo](src/main/resources/assets/mecha/icon.png)
+
+Multiple Entity Collision Hitboxes API
+
+## Setup
+
---
+++ src/main/java/com/bytemaniak/mecha/Mecha.java
@@ -0,0 +1,8 @@
+package com.bytemaniak.mecha;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mecha implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
